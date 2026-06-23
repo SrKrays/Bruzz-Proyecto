5a6eff4f483a138a7d3865868ed9a4cfccc7ca85
Mateo Cardenas <94235928+SrKrays@users.noreply.github.com>
2026-06-23 18:59:32 -0300

    chore: actualizar promosData.js [23/06/2026 21:59]

--- src/promosData.js
+++ src/promosData.js
@@ -1,4 +1,4 @@
-// promosData.js — Generado el 23/06/2026 21:50
+// promosData.js — Generado el 23/06/2026 21:59
 // ⚠️ No editar manualmente — usar el panel admin
 
 export const PROMOS = [
@@ -8,6 +8,14 @@ export const PROMOS = [
     desc: 'testeo',
     precio: '15',
     tag: 'A',
+    activa: true,
+  },
+  {
+    id: '4',
+    titulo: 'Pizza abduhsjbahf',
+    desc: 'testeo',
+    precio: 'qd11',
+    tag: '',
     activa: true,
   },
 ];
